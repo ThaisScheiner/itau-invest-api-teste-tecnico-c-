710d4b253ab6dcfc8a524cabf1bcc350754d6fd8
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2025-07-13 19:30:50 -0300

    feat(domain): Add core business entities

---
+++ ItauInvest.API/ItauInvest.API/Domain/Entities/Cotacao.cs
@@ -0,0 +1,10 @@
+﻿namespace ItauInvest.API.Domain.Entities
+{
+    public class Cotacao
+    {
+        public long Id { get; set; }
+        public long AtivoId { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}
---
+++ ItauInvest.API/ItauInvest.API/Domain/Entities/Operacao.cs
@@ -0,0 +1,14 @@
+﻿namespace ItauInvest.API.Domain.Entities
+{
+    public class Operacao
+    {
+        public long Id { get; set; }
+        public long UsuarioId { get; set; }
+        public long AtivoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public string TipoOperacao { get; set; } = null!; // "Compra" ou "Venda"
+        public decimal Corretagem { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}
