1123bc4faec9a3ef83ab7f382344f325c4628afb
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2025-07-13 19:31:05 -0300

    feat(app): Implement business logic, DTOs, and Kafka workers

---
+++ ItauInvest.API/ItauInvest.API/DTO/Rankings/TopCorretagemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ItauInvest.API.DTO.Rankings
+{
+    // DTO para o ranking de corretagens
+    public class TopCorretagemDto
+    {
+        public long UsuarioId { get; set; }
+        public decimal TotalCorretagem { get; set; }
+    }
+}
